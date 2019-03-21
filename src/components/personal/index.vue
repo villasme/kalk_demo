3cad0c46984fd7bd35cec147b699a30a0fa0f735
jiantong.liu <liujiantongvip@163.com>
2019-03-21 19:01:08 +0800

    计算

--- src/components/personal/index.vue
+++ src/components/personal/index.vue
@@ -1,6 +1,107 @@
 <template>
   <div class="personal">
-      personal
+       <div class="g-container">
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+          <div class="position"></div>
+       </div>
   </div>
 </template>
 
@@ -17,5 +118,33 @@ export default {
 }
 </script>
 
-<style lang="scss" module>
+<style lang="postcss" scoped>
+.g-container {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+}
+
+.position {
+  position: absolute;
+  width: 10vw;
+  height: 10vw;
+}
+
+@for $i from 0 to 100 {
+  // 当前是第几个盒子
+  $x: $i % 10;
+  // 当前个数 减去 当前是第几个 / 一行多少个
+  $y: ($i - $x) / 10;
+
+  .position:nth-child(calc($i+1)) {
+    background: red;
+    // top: $(y*10)vh;
+  // left: $x * 10vw;
+  }
+  .position:nth-child($i + 1):hover {
+    background: rgba(255, 155, 10, .5)
+  }
+}
+
 </style>
